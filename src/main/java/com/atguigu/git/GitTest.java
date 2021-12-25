844fe1bde9d3c8d4526b8e895b4975bdf02f8b0d
zhang <zhanghh97@126.com>
2021-12-25 12:23:28 +0800

    idea hot-fix 1 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git22222!");
         System.out.println("hello git33333333!");
+        System.out.println("hello git hot-fix");
     }
 }
