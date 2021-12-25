39a8240da3d513edac4ac58f2941f3cb31ea83f4
zhang <zhanghh97@126.com>
2021-12-25 13:29:04 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git hot-fix");
         System.out.println("hello git master hebingchongtu");
         System.out.println("hello git hot-fix chongtuhebing");
+        System.out.println("推送代码到远程库 push");
     }
 }
