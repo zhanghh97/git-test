14a2ab54b045b5a969bfe049b26ec8292e0231ed
zhang <zhanghh97@126.com>
2021-12-25 11:07:24 +0800

    idea 3 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git22222!");
+        System.out.println("hello git33333333!");
     }
 }
